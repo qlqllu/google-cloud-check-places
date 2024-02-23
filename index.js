c4b850af9a65472f049847a0de5d857516417ac6
Junshan Liu <junshan_liu@esri.com>
2024-02-23 17:31:48 +0800

    init

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const functions = require('@google-cloud/functions-framework');
-const axios = require('axios');
-const functions = require('@google-cloud/functions-framework');
 
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
